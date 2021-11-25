e822a5e41ef0216313ff41eace8e169c68439be6
Alessandro-Rega <regaale47@gmail.com>
2021-11-25 19:14:28 +0100

    insert script -> titolo + immagine

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const app = new Vue({
+        el: '#cont',
+        data: {
+            messaggio: "Ciao Mondo!!",
+            immagine: "https://codicicolori.com/images/IMM-BELLE/DASCARICARE/immagini-belle-da-scaricare-luna-mare.jpg"
+        },
+});
